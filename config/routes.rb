a0110e1ee51837488b1b42b55d771f8dbca44db5
EC2 Default User <ec2-user@ip-172-31-8-91.ec2.internal>
2022-11-21 05:55:11 +0000

    [add] users contoroller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
+  
   devise_for :users
   get 'homes/top',as:'routes'
   root to:"homes#top"
+  
+  get 'users/index'
+  get 'users/show'
+  get 'users/new'
+  get 'users/edit'
 end
