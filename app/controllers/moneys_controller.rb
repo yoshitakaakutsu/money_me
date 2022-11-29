c818221c52862e6b7835a2e343375aca0b3eaab7
EC2 Default User <ec2-user@ip-172-31-8-91.ec2.internal>
2022-11-29 08:08:25 +0000

    [Add] money_show

--- app/controllers/moneys_controller.rb
+++ app/controllers/moneys_controller.rb
@@ -3,12 +3,17 @@ class MoneysController < ApplicationController
     @payment = Payment.all
     @income = Income.all
     @chart = Payment.order('created_at ASC')
+    @charts = Income.order('created_at ASC')
   end
 
   def show
+    @chart = Payment.order('created_at ASC')
+    @charts = Income.order('created_at ASC')
   end
 
   def new
+    @payment = Payment.all
+    @income = Income.all
     @payment = Payment.new
     @income = Income.new
   end
